11b366df4e8f811298bd52c07f4ff8fe620c919d
Jaikesh Kain <164343591+Jaikeshkain@users.noreply.github.com>
2025-06-13 00:11:42 +0530

    Update EventService.ts

--- frontend/src/services/EventService.ts
+++ frontend/src/services/EventService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://eventify-mernstack.onrender.com';
 
 const CreateEventAPI = async (formData:FormData,token:string) => {
     try {
